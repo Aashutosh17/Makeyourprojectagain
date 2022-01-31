baacd53f2b119f3889765b6b06ca4365733f3e27
Aashutosh17 <78782279+Aashutosh17@users.noreply.github.com>
2022-01-31 08:53:06 +0545

    Displayproject page

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var menuIcon = document.querySelector(".menu-icon");
 var sidebar = document.querySelector(".sidebar");
 var container = document.querySelector(".container");
 
-menuIcon.oneclick = function(){
+menuIcon.onclick = function(){
     sidebar.classList.toggle("small-sidebar");
     container.classList.toggle("large-container");
 }
